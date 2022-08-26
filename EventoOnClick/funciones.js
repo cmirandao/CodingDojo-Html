a0ec5ac28a2fabd277f0db7bb2e80cd4ba074015
cmirandao <miranda.olivares@gmail.com>
2022-08-26 01:37:21 -0400

    Ejercicio Evento onclick

---
+++ EventoOnClick/funciones.js
@@ -0,0 +1,10 @@
+function likes(elemento) {
+    console.log("Ninja was liked", elemento);
+    alert("Ninja was liked");
+}
+function changeTxt(elemento){
+    elemento.innerText = "Logout";
+}
+function removeBtn(elemento){
+    elemento.remove();
+}
