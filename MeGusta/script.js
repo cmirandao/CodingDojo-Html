3bb5a37fe5ae6131952951e5f3c00cbaa26f6e6d
cmirandao <miranda.olivares@gmail.com>
2022-08-26 13:56:17 -0400

    Ejercicio Me Gusta

---
+++ MeGusta/script.js
@@ -0,0 +1,15 @@
+var counter1 = 9;
+var aaa = document.querySelector("div.usr_like p#like1");
+function like1() {
+    aaa.innerText = ++counter1 +" likes(s)";
+}
+var counter2 = 12;
+var bbb = document.querySelector("div.usr_like p#like2");
+function like2() {
+    bbb.innerText = ++counter2 +" likes(s)";
+}
+var counter3 = 9;
+var ccc = document.querySelector("div.usr_like p#like3");
+function like3() {
+    ccc.innerText = ++counter3 +" likes(s)";
+}
